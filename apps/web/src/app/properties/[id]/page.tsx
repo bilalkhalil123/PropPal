5f67e6e625503edd9ddbadbd19865e5383561c37
Rana Bilal Akbar <bilal.rana8718@gmail.com>
2025-10-28 15:13:34 +0500

    Property details API : Added

--- apps/web/src/app/properties/[id]/page.tsx
+++ apps/web/src/app/properties/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from 'next/navigation'
 import Link from 'next/link'
-import PropertyDetailPage from '@/components/PropertyDetailPage'
+import PropertyDetailPage from '../../../components/PropertyDetailPage'
 
 async function fetchProperty(id: string) {
   const base = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'
