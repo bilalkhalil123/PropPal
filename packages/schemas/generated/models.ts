90e3c50c0dd0644118943e302c95a1d3e3fea7fe
Rana Bilal Akbar <bilal.rana8718@gmail.com>
2026-04-01 03:37:12 +0500

    Readme Update

--- packages/schemas/generated/models.ts
+++ packages/schemas/generated/models.ts
@@ -1,7 +1,7 @@
 /**
  * AUTO-GENERATED TypeScript interfaces from Pydantic models
  * DO NOT EDIT MANUALLY - Changes will be overwritten
- * Generated on: 2026-03-30T22:50:40.360666
+ * Generated on: 2026-04-01T03:27:50.206407
  */
 
 export interface UserResponse {
